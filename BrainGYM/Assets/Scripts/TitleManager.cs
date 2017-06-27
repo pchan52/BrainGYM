53534f04c32afbc3d320865518445a4b2c6c6a0a
池田光 <Hikaru@pchan.local>
2017-06-27 21:05:40 +0900

    Create TitleManager.cs to manage button on title scene

---
+++ BrainGYM/Assets/Scripts/TitleManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void StartButton()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void RuleButton()
+	{
+		SceneManager.LoadScene("Rule");
+	}
+}
+
+}
