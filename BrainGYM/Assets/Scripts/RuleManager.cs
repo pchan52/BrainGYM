1d89cf97aeac76050a634e93a342518c82a9ec26
池田光 <Hikaru@pchan.local>
2017-06-27 22:52:15 +0900

    Create Rule scene UI

---
+++ BrainGYM/Assets/Scripts/RuleManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RuleManager : MonoBehaviour
+{
+
+	[SerializeField] private Text[] _rule;
+	[SerializeField] private GameObject _startButton;
+	[SerializeField] private GameObject _backButton;
+	[SerializeField] private float _time;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(ShowRuleCoroutine(_rule));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator ShowRuleCoroutine(Text[] texts)
+	{
+		foreach (Text t in texts)
+		{
+			yield return new WaitForSeconds(_time);
+			t.enabled = true;
+		}
+		
+		yield return new WaitForSeconds(_time);
+		_startButton.SetActive(true);
+		_backButton.SetActive(true);
+	}
+
+	public void StartButton()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void BackButton()
+	{
+		SceneManager.LoadScene("Title");
+	}
+}
--- BrainGYM/Assets/Scripts/TitleManager.cs
+++ BrainGYM/Assets/Scripts/TitleManager.cs
@@ -27,4 +27,3 @@ public class TitleManager : MonoBehaviour
 	}
 }
 
-}
