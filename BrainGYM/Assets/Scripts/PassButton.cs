b7639a12749336c90cb9913cb0cd2c661d80dafc
池田光 <Hikaru@pchan.local>
2017-06-29 14:11:30 +0900

    Create button systen on main scene

---
+++ BrainGYM/Assets/Scripts/PassButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PassNumberButton()
+	{
+		TargetNumberManager.instance.ReturnNumber();
+	}
+}
---
+++ BrainGYM/Assets/Scripts/PoseButton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PoseButton : MonoBehaviour
+{
+	[SerializeField] private Image _poseImage;
+	[SerializeField] private Image _playImage;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_poseImage.enabled = true;
+		_playImage.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GamePoseButton()
+	{
+		if (GameManager.instance.Isplaying)
+		{
+			print("aaa");
+			_playImage.enabled = true;
+			_poseImage.enabled = false;
+			GameManager.instance.Isplaying = false;
+		}
+		else
+		{
+			_playImage.enabled = false;
+			_poseImage.enabled = true;
+			GameManager.instance.Isplaying = true;
+		}
+	}
+}
