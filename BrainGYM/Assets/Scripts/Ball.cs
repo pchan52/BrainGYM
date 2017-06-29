a62163cec6b3a017795c63f5b589a712a6a986e2
池田光 <Hikaru@pchan.local>
2017-06-29 14:12:05 +0900

    Save scenes

--- BrainGYM/Assets/Scripts/Ball.cs
+++ BrainGYM/Assets/Scripts/Ball.cs
@@ -6,9 +6,9 @@ public class Ball : MonoBehaviour
 {
 
 	[SerializeField] private int _number;
-	[SerializeField] private bool _selected = false;
+	[SerializeField] private bool _selected;
+	[SerializeField] private TargetNumberManager _targetNumberManager;
 	
-
 	public int Number
 	{
 		get { return _number; }
@@ -21,22 +21,32 @@ public class Ball : MonoBehaviour
 		set { _selected = value; }
 	}
 
-
-	private void OnTriggerExit(Collider other)
-	{
-		if (other.gameObject.CompareTag("UpperBoundary"))
-		{
-			GetComponent<SpriteRenderer>().enabled = true;
-		}
-	}
-
 	// Use this for initialization
 	void Start ()
 	{
 		GetComponent<SpriteRenderer>().enabled = false;
+		_selected = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {	
+	}
+
+	public bool CheckSelectable()
+	{
+		var currentSum = SelectedBallListManager.instance.CalcListSum();
+		print(currentSum);
+		print(_number);
+		print(currentSum+_number);
+		print(TargetNumberManager.instance.TargetNumber);
+		if (currentSum + _number > TargetNumberManager.instance.TargetNumber) return false;
+		
+		return true;
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (!other.gameObject.CompareTag("UpperBoundary")) return;
+		GetComponent<SpriteRenderer>().enabled = true;
 	}
 }
