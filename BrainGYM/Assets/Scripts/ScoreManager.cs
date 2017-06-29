651a9479c5ac499de7ef031181416dff481e26de
池田光 <Hikaru@pchan.local>
2017-06-29 14:11:49 +0900

    Create score system

---
+++ BrainGYM/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+	[SerializeField] private ScoreRate _scoreRate;
+	[SerializeField] private Text _scoreText;
+	[SerializeField] private float _score;
+
+	// Use this for initialization
+	void Start()
+	{
+		_score = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_scoreText.text = _score.ToString();
+	}
+
+	public void CalcScore(List<GameObject> list)
+	{
+		var _rate = _scoreRate.Rate(list);
+		var _sum = list.Sum(obj => obj.GetComponent<Ball>().Number);
+		_score = _rate * _sum * 100;
+	}
+}
