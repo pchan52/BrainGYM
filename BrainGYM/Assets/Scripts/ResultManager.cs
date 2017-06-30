c614a74ef83daea33bd0aa3307b84a2b4a85992d
池田光 <Hikaru@pchan.local>
2017-06-30 23:46:23 +0900

    Edit script to sound each score

--- BrainGYM/Assets/Scripts/ResultManager.cs
+++ BrainGYM/Assets/Scripts/ResultManager.cs
@@ -1,19 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultManager : MonoBehaviour
 {
 	[SerializeField] private AudioClip _scoreSound;
 	[SerializeField] private Text[] _scoreTexts;
+	[SerializeField] private float _timeInterval;
 	private int[] _scores;
-	private Animator _animator;
 	private AudioSource _audioSource;
 	
 	// Use this for initialization
 	void Start () {
-		_animator = GetComponent<Animator>();
 		_audioSource = GetComponent<AudioSource>();
 		GetScores();
+		StartCoroutine(ShowScoreCoroutine());
 	}
 
 	private void GetScores()
@@ -23,13 +24,25 @@ public class ResultManager : MonoBehaviour
 		for (var i = 0; i < _scoreTexts.Length; i++)
 		{
 			_scoreTexts[i].text = _scores[i].ToString();
-			_audioSource.PlayOneShot(_scoreSound);
 		}
 		var index = _scores[_scoreTexts.Length];
 		if (index != -1)
 		{
 			_scoreTexts[index].color = Color.red;
 		}
-		_animator.SetTrigger("show");	
+	}
+
+	private IEnumerator ShowScoreCoroutine()
+	{
+		yield return new WaitForSeconds(_timeInterval);
+		_scoreTexts[2].enabled = true;
+		_audioSource.PlayOneShot(_scoreSound);
+		yield return new WaitForSeconds(_timeInterval);
+		_scoreTexts[1].enabled = true;
+		_audioSource.PlayOneShot(_scoreSound);
+		yield return new WaitForSeconds(_timeInterval);
+		_scoreTexts[0].enabled = true;
+		_audioSource.PlayOneShot(_scoreSound);
+		
 	}
 }
